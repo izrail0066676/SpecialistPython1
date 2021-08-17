c0b31b509a595592b8aff6bf742296e2fdeb319d
izrail0066676 <89096451+izrail0066676@users.noreply.github.com>
2021-08-17 20:35:29 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -4,4 +4,11 @@
 # а если делится на 3 и на 5 - вывести “Foobar”.
 # Для всех остальных случаев не выводить ничего.
 
-# TODO: your code here
+number = int(input())
+n = 15
+if 3 / n:
+    print("foo")
+if 5 / n:
+    print("bar")
+if 3 / n and 5 / n:
+    print("Foobar")
