cc8ad8ed36946bdd9164d985d8dc9fe2045a57a6
izrail0066676 <89096451+izrail0066676@users.noreply.github.com>
2021-08-20 20:17:05 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -11,3 +11,13 @@ print(max2(-10, -12))
 print(max2(2.5, 2.6))
 print(max2(-2.5, 0))
 print(max2(0, -2.5))
+
+
+def max2(n1, n2):
+    pass
+    return "{}/{}".format(n1, n2)
+print(max2(5, 6))
+print(max2(-10, -12))
+print(max2(2.5, 2.6))
+print(max2(-2.5, 0))
+print(max2(0, -2.5))
