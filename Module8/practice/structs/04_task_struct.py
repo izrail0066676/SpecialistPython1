367c3946e19595d1287aec04611fc95c4bb79fde
izrail0066676 <89096451+izrail0066676@users.noreply.github.com>
2021-08-26 20:44:18 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,8 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+
+
+dictionary_1.update(dictionary_2)
+print(dictionary_1)
+# посмотрел функцию обьединения и впринципе решил
